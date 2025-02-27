063d47036f6477010be037746b080c54798fd7f0
Pedro Avelino Epalanga <52350632+pedroavelin@users.noreply.github.com>
2025-02-27 20:24:04 +0000

    Update LoginPage.vue
     prepend-inner-icon="mdi-lock-outline"

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -2,28 +2,38 @@
   <v-container fluid class="d-flex justify-center align-center bg-light-blue-darken-4" style="height: 100vh;">
     <v-sheet rounded>
       <v-card elevation="10" class="mx-auto px-6 py-8" width="350">
+        <h3 class="text-subtitle-2 mb-4">Seja Bem vindo, preencha os campos abaixo para aceder a sua conta.</h3>
         <v-form @submit.prevent="login()">
 
           <v-text-field
             v-model="user.email"
+            prepend-inner-icon="mdi-email-outline"
             class="mb-2"
             label="Seu Email"
             clearable
             placeholder="Digite o seu email"
+            variant="underlined"
+            color="primary"
+            density="compact"
           ></v-text-field>
 
           <v-text-field
             v-model="user.password"
+             prepend-inner-icon="mdi-lock-outline"
             label="Palavra passe"
             placeholder="Digite a sua palavra passe"
+            variant="underlined"
+            color="primary"
+            density="compact"
             clearable
           ></v-text-field>
-<br>
+          <br>
           <v-btn
             color="success"
             size="large"
             type="submit"
             variant="flat"
+            density="compact"
             block
           >
             Entrar
